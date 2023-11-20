6c42d1e88665cb9751ceaedc61e3d8a5f5308185
DakshTyagi741 <92214423+DakshTyagi741@users.noreply.github.com>
2023-11-20 12:40:58 +0530

    bg image

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -4,15 +4,17 @@ import Navbar from '../../components/Navbar';
 import Create from './components/Create';
 import Search from './components/Search';
 import Status from './components/Status';
+import MyImage from './bg.jpg'
 // import Success from './components/Success';
 import Trip from './components/Trip';
 
 export default function Main() {
   const [option,setoption]=useState('Search');
   const [view,setView]=useState([false,false,false,false]);
+  const bgImg = `url(${MyImage})`;
   return (
     <>
-    <div className='w-100 '>
+    <div className='w-100 bg-image ' id='123' style={{ backgroundImage: bgImg }}>
     
     <Navbar/>;
     <div className='my-3 mx-auto w-50 border border-2 text-center p-2'>
